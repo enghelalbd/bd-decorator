5b55f6c9f3c14872659e274d21aa93b68c70febc
enghelalbd <informhelal@yahoo.com>
2025-12-18 12:49:31 +0600

    Map Done

--- src/Pages/coverage.jsx
+++ src/Pages/coverage.jsx
@@ -1,36 +1,41 @@
 import React from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { useLoaderData } from "react-router";
 
 const Coverage = () => {
-  const position = [51.505, -0.09];
+  const position = [23.685, 90.3563];
+  const serviceCenter = useLoaderData();
+  console.log(serviceCenter);
+
   return (
     <div>
-      <h2 className="text-5xl text-fuchsia-600 border w-full h-[800px]  ">
-        {" "}
-        We are all 64 District
-      </h2>
-      <div></div>
-      <div>
+      <h2 className="text-5xl text-fuchsia-600">We are all 64 District</h2>
+
+      <div className="border w-full h-[800px]">
         <MapContainer
           center={position}
           zoom={13}
           scrollWheelZoom={false}
-          className="h-[500px] w-full"
+          className="h-[800px]"
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+
+          {serviceCenter.map((center, index) => (
+            <Marker key={index} position={[center.latitude, center.longitude]}>
+              <Popup>
+                <strong>{center.district}</strong>
+                <br />
+                Service area :{center.covered_area.join()}
+              </Popup>
+            </Marker>
+          ))}
         </MapContainer>
       </div>
     </div>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+// import { createBrowserRouter } from "react-router";
+// import { RouterProvider } from "react-router/dom";
+import Coverage from "../Pages/Coverage";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello World</div>,
   },
+  {
+    path: "coverage",
+    Component: Coverage,
+    loader: () => fetch("/Servicecenter.json").then((res) => res.json()),
+  },
 ]);
 
-const root = document.getElementById("root");
+// const root = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+// ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+export default router;
